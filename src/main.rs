c73381be50e14ebeadfac121ebfe42fb7aa19f97
Herman J. Radtke III <herman@hermanradtke.com>
2016-01-29 18:44:16 -0800

    Working example
    The stockfighter create is imported and a call to the heartbeat API is
    made.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+extern crate stockfighter;
+
+use stockfighter::Stockfighter;
+
 fn main() {
-    println!("Hello, world!");
+    let sf = Stockfighter::new("my api key");
+
+    // heartbeat does not require a valid API key
+    sf.heartbeat().unwrap();
 }
